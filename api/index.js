e29ecbc62e82a1d59fd29973686f7ecc1ec02c1a
Jinjun Liang <kumkeejjl@gmail.com>
2022-12-01 13:55:50 +1300

    combined two const definitions

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
-const express = require('express')
+const app = require('express')()
 const ordbook = require('./orderbook')
-const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
